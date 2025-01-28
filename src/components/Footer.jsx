054532adf1bcfbf3b11c00a4218fbb051c927a57
Cyrus748 <adityanegi748@gmail.com>
2025-01-28 23:02:33 +0530

    Update Repository

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='text-center py-2 border-t-2 border-t-gray-500 '>&copy; 2025 VibeCoin | Aditya Negi</div>
+    )
+}
+
+export default Footer
